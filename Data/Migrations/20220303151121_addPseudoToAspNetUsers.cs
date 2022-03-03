bb81c991dcaa16ac0a686835cf9d61a39ecbb8a8
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-03-03 16:23:26 +0100

    create migration : add the pseudo row to the user table

---
+++ Data/Migrations/20220303151121_addPseudoToAspNetUsers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace onliga.Data.Migrations
+{
+    public partial class addPseudoToAspNetUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+            name: "Pseudo",
+            table: "AspNetUsers",
+            nullable: false,
+            maxLength: 20);
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
