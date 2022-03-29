fe466cd76103669ae16f6dc0372151e2f7766fc1
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-03-29 22:51:17 +0200

    add test view

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -22,9 +22,13 @@ namespace onliga.Controllers
         // 
         // GET: /book/Welcome/ 
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
+
+        public IActionResult Test(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
