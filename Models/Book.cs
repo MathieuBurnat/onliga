c5e26d695c101cae1cc0d6050243824f84f09f02
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-03-31 01:11:15 +0200

    Create validation

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,19 +1,19 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+
 namespace onliga.Models
 {
     public class Book
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
     }
 }
