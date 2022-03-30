dc80196b7997255a87b25a6b84e8395678d96b89
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-03-30 21:17:14 +0200

    Create 20220330191400_Manga.cs

---
+++ Data/Migrations/20220330191400_Manga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using onliga.Models;
+
+namespace onliga.Data
+{
+    public class onligaContext : DbContext
+    {
+        public onligaContext (DbContextOptions<onligaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<onliga.Models.Book> Book { get; set; }
+    }
+}
