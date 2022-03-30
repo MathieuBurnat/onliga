9cbaac50af1e37366577a292d47865c3326f4ea5
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-03-30 22:08:44 +0200

    Avoid "no migration found" error

--- Data/Migrations/20220330200500_books.Designer.cs
+++ Data/Migrations/20220330200500_books.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using onliga.Data;
 
@@ -10,9 +11,10 @@ using onliga.Data;
 namespace onliga.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220330200500_books")]
+    partial class books
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.3");
---
+++ Data/Migrations/20220330200500_books.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace onliga.Data.Migrations
+{
+    public partial class books : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                           name: "Books",
+                           columns: table => new
+                           {
+                               Id = table.Column<int>(type: "INTEGER", nullable: false)
+                                   .Annotation("Sqlite:Autoincrement", true),
+                               Title = table.Column<string>(type: "TEXT", nullable: true),
+                               ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                               Genre = table.Column<string>(type: "TEXT", nullable: true)
+                           },
+                           constraints: table =>
+                           {
+                               table.PrimaryKey("PK_Books", x => x.Id);
+                           });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
